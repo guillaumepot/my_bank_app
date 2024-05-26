155b76795581bee662588079590d0de30042e155
guillaumepot <guillaume.pot@outlook.com>
2024-05-26 17:09:12 +0200

    Update Scripts

--- src/api/remove_api.sh
+++ src/api/remove_api.sh
@@ -2,7 +2,7 @@
 
 
 # Stop docker compose
-docker-compose down
+docker compose down
 
 # Remove image
 docker image rm personal_bank_app_api:latest
--- src/streamlit/remove_streamlit.sh
+++ src/streamlit/remove_streamlit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Stop docker compose
-docker-compose down
+docker compose down
 
 # Remove image
 docker image rm personal_bank_app_streamlit:latest
